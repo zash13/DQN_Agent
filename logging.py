1d05e188b764eeb5b338b3a02030aa1259f67a25
k13 <mihome013@gmail.com>
2025-07-28 22:32:56 +0330

    complite logging

--- logging.py
+++ logging.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import enum
 from typing import Optional
 
+from numpy import log
+
 
 class LogLevel(Enum):
     INFO = auto()
@@ -30,7 +32,7 @@ class Logging:
         self.file_name = file_name
         self.verbos = verbosity
 
-    def log(
+    def _log(
         self,
         log_level: LogLevel,
         message: str,
@@ -38,28 +40,50 @@ class Logging:
         *args,
         **keywords,
     ):
-        pass
+        if self.verbos == Verbosity.INFO and log_level.value > self.verbos.value:
+            return
+        formatted_message = self._formatting_message(
+            log_level, message, *args, **keywords
+        )
+
+        self._write_to_file(formatted_message)
+        if show_in_console:
+            self._write_to_console(formatted_message)
 
     def _formatting_message(
         self, log_level: LogLevel, message: str, *args, **keywords
     ) -> str:
-        return ""
+        formatted_content = message.format(*args, **keywords)
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        return f"[{timestamp}] [{log_level.name}] {self.name} - {formatted_content}"
 
     def _write_to_console(self, message: str) -> None:
-        pass
+        print(message)
 
     def _write_to_file(self, message: str) -> None:
-        pass
+        with open(self.file_name, "a", encoding="utf-8") as f:
+            f.write(message)
 
-    def info(self, message: str, *args, **keywords) -> None:
-        pass
+    def info(
+        self,
+        message: str,
+        show_in_console: bool = False,
+        *args,
+        **keywords,
+    ) -> None:
+        self._log(LogLevel.INFO, message, show_in_console, *args, **keywords)
 
-    def debug(self, message: str, *args, **keywords) -> None:
-        pass
+    def debug(
+        self, message: str, show_in_console: bool = False, *args, **keywords
+    ) -> None:
+        self._log(LogLevel.DEBUG, message, show_in_console, *args, **keywords)
 
-    def warning(self, message: str, *args, **keywords) -> None:
-        pass
+    def warning(
+        self, message: str, show_in_console: bool = False, *args, **keywords
+    ) -> None:
+        self._log(LogLevel.WARNING, message, show_in_console, *args, **keywords)
 
-    def error(self, message: str, *args, **keywords) -> None:
-        pass
-
+    def error(
+        self, message: str, show_in_console: bool = False, *args, **keywords
+    ) -> None:
+        self._log(LogLevel.ERROR, message, show_in_console, *args, **keywords)
